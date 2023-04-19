90079a1c65a883858c05bd074eb9361e5b4fce8e
jruanova1987 <jorge.r@saffire.com>
2023-04-19 14:51:59 -0300

    test nest3

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         nested: resolve('src/pages', 'contact.html'),
-        nested2: resolve('src/pages', 'about.html'),
+        nested2: resolve('src/pages', 'About.html'),
       },
     },
   },
