f6eb1508039402a5b128e112dd1184658b55d9c5
jruanova1987 <jorge.r@saffire.com>
2023-04-19 15:11:37 -0300

    test structure-5

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import './public/sass/main.scss'
+import '/sass/main.scss'
 
 import $ from "jquery";
 // import './styles.css'
 
-import widget_a from './public/js/widgets/widget_a.js'
-import widget_b from './public/js/widgets/widget_b.js'
-import widget_c from './public/js/widgets/widget_c.js'
-import widget_d from './public/js/widgets/widget_d.js'
-import widget_e from './public/js/widgets/widget_e.js'
+import widget_a from './js/widgets/widget_a.js'
+import widget_b from './js/widgets/widget_b.js'
+import widget_c from './js/widgets/widget_c.js'
+import widget_d from './js/widgets/widget_d.js'
+import widget_e from './js/widgets/widget_e.js'
 
-import './public/js/default.js';
+import './js/default.js';
 
 
 // document.querySelector('#app').innerHTML = ``
