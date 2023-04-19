5194f949fbc81b2d1178fefb270809ab345842b5
jruanova1987 <jorge.r@saffire.com>
2023-04-19 14:33:39 -0300

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        pages1: resolve('src/pages', 'Contact.html'),
-        pages2: resolve('src/pages', 'Products.html'),
-        pages3: resolve('src/pages', 'About.html'),
+        contact: resolve('src/pages', 'Contact.html'),
+        products: resolve('src/pages', 'Products.html'),
+        about: resolve('src/pages', 'About.html'),
       },
     },
   },
