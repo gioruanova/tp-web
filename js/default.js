f4cc90588f891da1277e3099e46ab2ebe2ef1292
jruanova1987 <jorge.r@saffire.com>
2023-04-19 22:02:08 -0300

    theme

--- js/default.js
+++ js/default.js
@@ -1,5 +1,34 @@
-
+import $ from "jquery";
 
 
 console.log("default Widget");
 
+// Theme Selector
+$(document).ready(function () {
+    let currentTheme = localStorage.getItem('themeSelected')
+
+    if (currentTheme == null) {
+        localStorage.setItem('themeSelected', 'darkMode');
+        $('body').removeClass('lightMode')
+        $('#theme-selector').removeClass('update')
+
+    } else if (currentTheme == 'darkMode') {
+        $('body').removeClass('lightMode')
+        $('#theme-selector').removeClass('update')
+    } else {
+        $('body').addClass('lightMode')
+        $('#theme-selector').addClass('update')
+    }
+
+    $('.selector-container').on('click', function () {
+        if ($('body').hasClass('')) {
+            $('body').addClass('lightMode')
+            $('#theme-selector').addClass('update')
+            localStorage.setItem('themeSelected', 'lightMode');
+        } else {
+            $('body').removeClass('lightMode')
+            $('#theme-selector').removeClass('update')
+            localStorage.setItem('themeSelected', 'darkMode');
+        }
+    })
+});
