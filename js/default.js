dea944b6d30b83ef49695cc52de892bbb1fb3b13
jruanova1987 <jorge.r@saffire.com>
2023-05-03 18:34:09 -0300

    page updates

--- js/default.js
+++ js/default.js
@@ -11,24 +11,30 @@ $(document).ready(function () {
         localStorage.setItem('themeSelected', 'darkMode');
         $('body').removeClass('lightMode')
         $('#theme-selector').removeClass('update')
+        $('#theme-selector2').removeClass('update')
 
     } else if (currentTheme == 'darkMode') {
         $('body').removeClass('lightMode')
         $('#theme-selector').removeClass('update')
+        $('#theme-selector2').removeClass('update')
     } else {
         $('body').addClass('lightMode')
         $('#theme-selector').addClass('update')
+        $('#theme-selector2').addClass('update')
     }
 
     $('.selector-container').on('click', function () {
         if ($('body').hasClass('')) {
             $('body').addClass('lightMode')
             $('#theme-selector').addClass('update')
+            $('#theme-selector2').addClass('update')
             localStorage.setItem('themeSelected', 'lightMode');
         } else {
             $('body').removeClass('lightMode')
             $('#theme-selector').removeClass('update')
+            $('#theme-selector2').removeClass('update')
             localStorage.setItem('themeSelected', 'darkMode');
+            
         }
     })
 });
@@ -38,11 +44,11 @@ $(document).ready(function () {
 $(document).ready(function () {
 
     $('#burger-button').on('click', function () {
-        $(this).toggleClass('menu-open')
-        let buttonBurger = $('#burger-button')
-        $('.items').toggleClass('showItems')
-        $('.container-nav').toggleClass('full-width')
+        $('.menu-mobile').toggleClass('menu-open')
+        $('.burger-container').toggleClass('menu-open')
     })
+
+    $('.activeClass').removeAttr('href')
 
 })
 
