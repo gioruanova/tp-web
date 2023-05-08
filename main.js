e5b306de0c4d2224af918858d84c4fb6313a7e7e
jruanova1987 <jorge.r@saffire.com>
2023-05-07 21:50:39 -0300

    footer

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ import widget_d from './js/widgets/widget_d.js'
 import widget_e from './js/widgets/widget_e.js'
 
 import './js/default.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'animate.css';
 
 
 // document.querySelector('#app').innerHTML = ``
@@ -23,6 +26,13 @@ if (window.location.pathname === '/') {
         widget_d();
         widget_e();
         $('.testing-class').addClass('background-change')
+        
+            AOS.init({
+              anchorPlacement: 'bottom-bottom',
+              animatedClassName: 'aos-animate',
+              initClassName: 'aos-init',
+            });
+        
         
     })
 }
